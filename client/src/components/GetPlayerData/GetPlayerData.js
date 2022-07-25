d10edf7739295eb99e1c275c067cc48c5754446c
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-25 11:49:31 +0530

    Fetching and displaying the players

--- client/src/components/GetPlayerData/GetPlayerData.js
+++ client/src/components/GetPlayerData/GetPlayerData.js
@@ -1,17 +1,37 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 
 const GetPlayerData = () => {
+  const [players, setPlayers] = useState([]);
   const fetchPlayer = () => {
     axios
       .get("http://localhost:4000/admin/get-player")
-      .then((res) => console.log(res));
+      .then((res) => {
+        if (res.status === 200) {
+          setPlayers(res.data.players);
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   };
   return (
     <div>
       <button type="button" onClick={fetchPlayer}>
         Get Player Data
       </button>
+      {players.length > 0 &&
+        players.map((player) => (
+          <div key={player._id}>
+            <h1>{player.name}</h1>
+            <div>
+              <h1>Innings: {player.innings}</h1>
+              <h2>Average: {player.average}</h2>
+              <span>Runs: {player.runs}</span>
+              <span>Centuries: {player.centuries}</span>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
